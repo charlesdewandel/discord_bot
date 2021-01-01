fb7d8cada1c95fa3f1dda7feaaa773d4965918f0
charlesdewandel <67976464+charlesdewandel@users.noreply.github.com>
2021-01-01 11:11:46 +0100

    version '1.0.1'
    update

---
+++ bot copy.py
@@ -0,0 +1,37 @@
+# Work with Python 3.6
+import discord
+
+TOKEN = 'your token'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    # dialogues
+    if message.content.startswith('salut'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.author.send('👋')
+        await message.channel.send("salut")
+    if message.content.startswith('fdp'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send("parle mieux où je t'éjectes !")
+    if message.content.startswith('Alz'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send("voilà soumets-toi")
+    if message.content.startswith('sur fortnite'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send("warzone est mieux")
+    
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
